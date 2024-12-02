734de8393cf80fa7ec88c868f74139df1c156e98
davidgw <coreax7@gmail.com>
2024-12-02 19:11:20 +0900

    ㅇㅇ

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,23 @@
-import Image from "next/image";
-import img01 from "/public/images/coffee-blue.jpg"
+// import Image from "next/image";
+// import img01 from "/public/images/coffee-blue.jpg"
 // page.js는 필수이다. (생략 불가)
 // 각 경로(/, /about, /content ..) 마다 페이지를 렌더링하려면
 // 해당 경로의 page.js 파일이 반드시 필요하다.
 
-
+import itemList from './itemList/page'
 // 자식컴포넌트
 export default function Home() {
   return (
     // 해당 내용은 부모컴포넌트의 props => {children} 에 삽입된다.
     <>
-    <h1>WelCome</h1>
+    <h1>welcome</h1>
     {/* 이미지 자체를 import 하지 않으면 너비와 높이를 넣어줘야 한다. */}
     {/* <p><Image src="/images/coffee-blue.jpg" alt="" width={100} height={100} /></p> */}
 
     {/* 너비 높이는 선택 사항 */}
     {/* <p><Image src={img01} alt="" /></p> */}
     {/* <p><Image src={img01} alt="" width={100} height={100}/></p> */}
+    
     </>
   );
 }
